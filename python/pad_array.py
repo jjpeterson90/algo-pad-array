3268ff85cd3cd2ceeacfa54b12c51965d2d40c8a
Justin <jjpeterson90@hotmail.com>
2022-05-28 13:53:32 -0500

    complete

--- python/pad_array.py
+++ python/pad_array.py
@@ -1,2 +1,14 @@
 #REMEMBER TO PSEUDOCODE
 def pad(array, min_size, value = None):
+    if min_size > len(array):
+        for i in range(min_size-len(array)):
+            array.append(value)
+    else:
+        return array
+    
+    return array
+
+
+# print(pad([1,2,3], 5))
+# print(pad([1,2,3], 5, 'apple'))
+# print(pad([1,2,3], 1))
