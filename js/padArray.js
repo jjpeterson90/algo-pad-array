419eae628c003781d279fa24b0afb6adefe8a871
Justin <jjpeterson90@hotmail.com>
2022-05-29 14:05:22 -0500

    complete

--- js/padArray.js
+++ js/padArray.js
@@ -1,4 +1,12 @@
 // REMEMBER TO PSEUDOCODE
 const pad = (array, minSize, value=null) => {
-
+    if (minSize <= array.length) return array
+    padCount = minSize - array.length;
+    for (let i = 1; i <= padCount; i++) {
+        array.push(value)
+    }
+    return array
 }
+
+module.exports = { pad }
+
--- js/padArraySpec.js
+++ js/padArraySpec.js
@@ -1,1 +1,8 @@
 // Write unit tests!
+let pd = require('./padArray')
+
+console.log(pd.pad([1,2,3], 5)) // == [1,2,3,None,None])
+console.log(pd.pad([1,2,3], 5, 'apple')) // == [1,2,3,'apple','apple'])
+console.log(pd.pad([1,2,3], 3)) // == [1,2,3])
+console.log(pd.pad([1,2,3], 1)) // == [1,2,3])
+console.log(pd.pad([1,2,3], 0, 'apple')) // == [1,2,3])
